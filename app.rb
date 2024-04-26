f7ca316393b590a2a7a00223814d63c039fbf678
m-nt-s-no <45675504+m-nt-s-no@users.noreply.github.com>
2024-04-26 13:22:53 +0000

    rough outline of app.rb

---
+++ app.rb
@@ -0,0 +1,15 @@
+require "http"
+require "json"
+
+#Here is a suggested outline for your program:
+#Ask the user for their location. (Recall gets.)
+#Get and store the user’s location.
+#Get the user’s latitude and longitude from the Google Maps API.
+#Get the weather at the user’s coordinates from the Pirate Weather API.
+#Display the current temperature and summary of the weather for the next hour.
+
+#If you get that far, then stretch further:
+#For each of the next twelve hours, check if the precipitation probability is greater than 10%.
+#If so, print a message saying how many hours from now and what the precipitation probability is.
+#If any of the next twelve hours has a precipitation probability greater than 10%, print “You might want to carry an umbrella!”
+#If not, print “You probably won’t need an umbrella today.”
