4201159745db3d164eee63ea0b2ab1a7537b4410
m-nt-s-no <45675504+m-nt-s-no@users.noreply.github.com>
2024-04-29 13:29:13 +0000

    loaded environment variables, tested them on rough draft of program

--- app.rb
+++ app.rb
@@ -1,12 +1,37 @@
 require "http"
 require "json"
 
+#got the idea to require and load dotenv from Vonage developer website:
+#https://developer.vonage.com/en/blog/working-with-environment-variables-in-ruby
+require "dotenv"
+Dotenv.load
+#also had to run this in terminal: gem install dotenv
+
+gmaps_key = ENV.fetch("GMAPS_KEY")
+pirate_weather_key = ENV.fetch("PIRATE_WEATHER_KEY")
+
 #Here is a suggested outline for your program:
 #Ask the user for their location. (Recall gets.)
 #Get and store the user’s location.
+
+puts "Where are you?"
+location = gets.chomp
+
 #Get the user’s latitude and longitude from the Google Maps API.
+
+
 #Get the weather at the user’s coordinates from the Pirate Weather API.
+pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_key + "/41.8887,-87.6355"
+
+parsed_response = JSON.parse(HTTP.get(pirate_weather_url))
+
+currently_hash = parsed_response.fetch("currently")
+
+current_temp = currently_hash.fetch("temperature")
+
 #Display the current temperature and summary of the weather for the next hour.
+puts "The current temperature at #{location } is #{current_temp.to_s} Fahrenheit."
+
 
 #If you get that far, then stretch further:
 #For each of the next twelve hours, check if the precipitation probability is greater than 10%.
